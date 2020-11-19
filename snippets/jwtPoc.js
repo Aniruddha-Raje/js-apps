6a897b4ce2bbe396bca4f46abfded83d2a40a050
Aniruddha Raje <aniruddha.raje@blazeclan.com>
2020-11-19 09:39:29 +0530

    jwt poc

---
+++ snippets/jwtPoc.js
@@ -0,0 +1,5 @@
+const jwt = require('jsonwebtoken');
+
+let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+console.log(JSON.stringify(jwt.decode(token)));
